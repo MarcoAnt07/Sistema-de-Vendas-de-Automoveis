d81e62eb8515bc28d0743600bfcd472ad702c857
Marco Antônio <120525664+MarcoAnt07@users.noreply.github.com>
2023-06-13 19:27:17 -0300

    1.0.1

--- js/autos.js
+++ js/autos.js
@@ -2,7 +2,6 @@ let titulo = localStorage.getItem('titulo');
 
 document.getElementById('titulo').innerHTML = titulo
 
-//localStorage.removeItem('titulo')
 function vendas(){
     window.location.href = 'vendas.html'
 }
